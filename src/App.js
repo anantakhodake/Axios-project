d24de4a6ddf5f95b89abea5fcf0672e616a5fe33
ananta khodake <ananta.khodake02@gmail.com>
2023-12-26 15:18:25 +0530

    get the data succesfully using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect,useState } from 'react';
+import axios from 'axios'
 function App() {
+const[myData,setMyData] = useState([]);
+  // using Promises
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then((resp)=>
+    setMyData(resp.data)
+    );
+  },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h2>Axios Tutorial</h2>
+    <div className='grid'>
+    {myData.map((post)=>{
+      const {id,title,body} = post;
+      return <div className='card' key={id}>
+        <h2>{title}</h2>
+        <p>{body}</p>
+      </div>
+    })}
     </div>
+    </>
   );
 }
-
 export default App;
